15424e7de4aad3896a14b614f5375834bd616949
plus4academy <plus4academy2025@gmail.com>
2026-02-10 17:07:21 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -31,8 +31,9 @@ export default function Gallery() {
     { id: 2, caption: "Doubt Solving" },
     { id: 3, caption: "Test Series" },
     { id: 4, caption: "One-on-One Mentoring" },
-    { id: 5, caption: "Study Materials" },
+    { id: 5, caption: "Our Trips" },
     { id: 6, caption: "Student Success Stories" },
+    { id: 7, caption: "Celebrations" },
   ];
 
   return (
