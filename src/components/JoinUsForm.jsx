3f3eca9d2f92cae4a7260898994aad04114e3f31
plus4academy <plus4academy2025@gmail.com>
2026-04-09 15:14:48 +0530

    Update JoinUsForm.jsx
    fix

--- src/components/JoinUsForm.jsx
+++ src/components/JoinUsForm.jsx
@@ -92,7 +92,7 @@ export default function JoinUsForm() {
         <div className="form-wrapper">
           <h2 className="form-title">Join Plus 4 Academy</h2>
           <p className="form-subtitle">
-            Enquire Now & Get 10% Instant Scholarship On All The Courses.
+           Enquire Now & someone from our team will contact you soon!
           </p>
 
           
